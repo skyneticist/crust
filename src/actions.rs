de6e83df8addba563a801cb41fb3ffe06196a5c2
skyneticist, teslatickles <hunterhartline87@gmail.com>
2021-11-10 18:58:57 -0600

    clean up monolith main.rs file -- types.rs, actions.rs, main.rs minimal

--- src/actions.rs
+++ src/actions.rs
@@ -1,4 +1,59 @@
-// this file contains all of the functions/actions
-// used for cli app
+use crate::types::RootCmd::Git;
+use crate::types::GitCommands::{Add, Commit, Log, Push, Status};
+use crate::types::*;
+use std::process::Command;
 
-fn add_commit_push() {}
+pub fn run_git_cmd(arg: GitCommands, sub_args: Option<Vec<String>>) -> String {
+    let sub_args = match sub_args {
+        Some(values) => values,
+        None => Vec::new(),
+    };
+
+    let output = Command::new(Git.value())
+        .arg(arg.value())
+        .args(sub_args)
+        .output()
+        .expect("error at run_git_cmd");
+
+    match String::from_utf8(output.stdout) {
+        Ok(output_str) => output_str,
+        Err(_) => String::from("Error has occurred at from_utf8 method"),
+    }
+}
+
+pub fn add_commit_push(remote: Option<bool>, commit_msg: String) -> String {
+    run_git_cmd(Add, Some(vec![String::from(".")]));
+
+    let mut sub_args = Vec::new();
+    sub_args.push(String::from("-m"));
+    sub_args.push(commit_msg);
+    run_git_cmd(Commit, Some(sub_args));
+
+    let is_fresh = match remote {
+        Some(value) => value,
+        None => false,
+    };
+
+    if is_fresh {
+        let mut remote_push_args = Vec::new();
+        remote_push_args.push(String::from("-u"));
+        remote_push_args.push(String::from("origin"));
+        remote_push_args.push(String::from("HEAD"));
+
+        return run_git_cmd(Push, Some(remote_push_args));
+    }
+
+    return String::from("");
+}
+
+pub fn get_status() -> String {
+    return run_git_cmd(Status, None);
+}
+
+pub fn log_commits(pithy: bool, dump: bool, filter: Option<String>) -> String {
+    let mut sub_cmd: Vec<String> = Vec::new();
+    if pithy {
+        &sub_cmd.push(String::from("--pretty=oneline"));
+    }
+    return run_git_cmd(Log, Some(sub_cmd));
+}
--- src/types.rs
+++ src/types.rs
@@ -1,1 +1,48 @@
+pub enum RootCmd {
+    Git,
+}
 
+impl RootCmd {
+    pub fn value(&self) -> String {
+        match *self {
+            RootCmd::Git => String::from("git"),
+        }
+    }
+}
+
+// #[derive(StructOpt)]
+pub enum GitCommands {
+    Add,
+    Commit,
+    Log,
+    Push,
+    Pull,
+    Stash,
+    Revert,
+    Pop,
+    Apply,
+    Reset,
+    Hard,
+    Soft,
+    Status,
+}
+
+impl GitCommands {
+    pub fn value(&self) -> String {
+        match *self {
+            GitCommands::Add => String::from("add"),
+            GitCommands::Commit => String::from("commit"),
+            GitCommands::Log => String::from("log"),
+            GitCommands::Push => String::from("push"),
+            GitCommands::Pull => String::from("pull"),
+            GitCommands::Stash => String::from("stash"),
+            GitCommands::Reset => String::from("reset"),
+            GitCommands::Revert => String::from("revert"),
+            GitCommands::Pop => String::from("pop"),
+            GitCommands::Apply => String::from("apply"),
+            GitCommands::Hard => String::from("hard"),
+            GitCommands::Soft => String::from("soft"),
+            GitCommands::Status => String::from("status"),
+        }
+    }
+}
