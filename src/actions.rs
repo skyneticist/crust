eed56669a6e7200319a9e0fce3f6abaa5da52f2f
skyneticist, teslatickles <hunterhartline87@gmail.com>
2021-11-11 00:55:16 -0600

    satisfy clippy

--- src/actions.rs
+++ src/actions.rs
@@ -24,36 +24,26 @@ pub fn run_git_cmd(arg: GitCommands, sub_args: Option<Vec<String>>) -> String {
 pub fn add_commit_push(remote: Option<bool>, commit_msg: String) -> String {
     run_git_cmd(Add, Some(vec![String::from(".")]));
 
-    let mut sub_args = Vec::new();
-    sub_args.push(String::from("-m"));
-    sub_args.push(commit_msg);
+    let sub_args = vec![String::from("-m"), commit_msg];
     run_git_cmd(Commit, Some(sub_args));
 
-    let is_fresh = match remote {
-        Some(value) => value,
-        None => false,
+    let is_fresh = remote.unwrap_or(false);
+    let remote_push_args = match is_fresh {
+        true => vec![String::from("-u"), String::from("origin"), String::from("HEAD")],
+        false => vec![],
     };
 
-    if is_fresh {
-        let mut remote_push_args = Vec::new();
-        remote_push_args.push(String::from("-u"));
-        remote_push_args.push(String::from("origin"));
-        remote_push_args.push(String::from("HEAD"));
-
-        return run_git_cmd(Push, Some(remote_push_args));
-    }
-
-    return String::from("");
+    return run_git_cmd(Push, Some(remote_push_args));
 }
 
 pub fn get_status() -> String {
     return run_git_cmd(Status, None);
 }
 
-pub fn log_commits(pithy: bool, dump: bool, filter: Option<String>) -> String {
+pub fn log_commits(pithy: bool, _dump: bool, _filter: Option<String>) -> String {
     let mut sub_cmd: Vec<String> = Vec::new();
     if pithy {
-        &sub_cmd.push(String::from("--pretty=oneline"));
+        sub_cmd.push(String::from("--pretty=oneline"));
     }
     return run_git_cmd(Log, Some(sub_cmd));
 }
